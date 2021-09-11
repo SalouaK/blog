216aa5900a11bf3c32882129d14ee30fd3f9f10e
SalouaK <saloua.khettab@gmail.com>
2021-09-11 19:02:40 +0200

    All issues fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,22 @@
+//* if you want to use lodash look at the code in comment without highlight 
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+//* to make a unique request for each usersIds
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+   await dispatch(fetchPosts());
+
+//    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+//    userIds.forEach(id => dispatch(fetchUser(id)));
+
+   //* to refactor the const userIds 
+   _.chain(getState().posts)
+   .map('userId')
+   .uniq()
+   .forEach(id => dispatch(fetchUser(id)))
+   .value()
+};
+
 
 // * a function (fetchPosts) that return another fonction
 export const fetchPosts = () => async (dispatch) => {
@@ -6,6 +24,31 @@ export const fetchPosts = () => async (dispatch) => {
 
     dispatch({
         type: 'FETCH_POSTS',
-        playload: response.data
+        payload: response.data
     });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+};
+
+// export const fetchUser = (id) => (dispatch) => {
+//     _fetchUser(id, dispatch);
+// };
+
+//* we can also write this like this 
+//* export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     });
+// });
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,24 +1,46 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import {  fetchPostsAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
-const PostList = ({posts}) => {
+const PostList = ({ posts, fetchPostsAndUsers}) => {
 
     useEffect(() => {
-        fetchPosts();
-    });
-    console.log(posts)
+        fetchPostsAndUsers();
+    }, [fetchPostsAndUsers]);
+
+
+    const renderList = () => {
+        return posts.map(post => {
+            return (
+                <div className='item' key={post.id}>
+                    <i className='large middle aligned icon user' />
+                    <div className='content'>
+                        <h2>
+                            {post.title}
+                        </h2>
+                        <p>
+                            {post.body}
+                        </p>
+                    </div>
+                    <UserHeader userId={post.userId} />
+                </div>
+            )
+        })
+
+    }
+
     return (
-        <div>
-            Post List 
+        <div className='ui relaxed divided list'>
+            {renderList()}
         </div>
     );
-    
+
 };
 
 const mapStateToProps = (state) => {
     return { posts: state.posts };
-    
+
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
